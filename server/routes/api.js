9da2aea37c28f5a1119a2c2e3822291a83e2626e
Jase Cutler <jasecutler@gmail.com>
2017-09-21 16:19:32 -0700

    fixing typo

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,7 @@ var twilio = require('twilio')
 /* /api starting endpoint */
 router.post('/sms', function(req, res, next) {
   console.log(req.body.Body)
-  var twilio = require('twilio');
+  console.log("req body", req.body)
   var twiml = new twilio.TwimlResponse();
   twiml.message('The Robots are coming! Head for the hills!');
   res.writeHead(200, {'Content-Type': 'text/xml'});
