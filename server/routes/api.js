ec828e429f93c7524bab19e9999b3fa7bfaaa8c7
Jase Cutler <jasecutler@gmail.com>
2017-09-21 15:51:25 -0700

    Adding in twilio bits

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var twilio = require('twilio')
 
 /* /api starting endpoint */
-router.get('/foo', function(req, res, next) {
-  res.json({
-    foo: 'bar'
-  })
+router.post('/sms', function(req, res, next) {
+  console.log(res.body)
+  var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 module.exports = router;
