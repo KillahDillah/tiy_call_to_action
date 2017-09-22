dbe220b27b5aa8c311dc9118824f3b8e39da32b8
Jase Cutler <jasecutler@gmail.com>
2017-09-22 13:29:00 -0700

    Fixed issue with promise

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,11 +4,17 @@ var twilio = require('twilio')
 const Text = require('../model/textModel')
 
 /* /api starting endpoint */
-router.post('/sms', function(req, res, next) {
-  Text.insertText(req.body.Body)
+router.post('/sms', function (req, res, next) {
+  let text = Text.insertText(req.body.Body)
+  text.catch(function (err) {
+    res.json(err)
+  })
+    .then(function (data) {
+      res.json(data)
+    })
   var twiml = new twilio.TwimlResponse();
   twiml.message('The Robots are coming! Head for the hills!');
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
 });
 
