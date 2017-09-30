26476eeb93e4cb4525fe650d41e61cb7136b1b81
Leah Miller <leahmiller1969@gmail.com>
2017-09-29 23:39:40 -0700

    send button leading back to dashboard added. map to add campaign back-end info

--- client/src/components/Letter.js
+++ client/src/components/Letter.js
@@ -1,39 +1,64 @@
 import React, { Component } from 'react'
+import jwtDecode from 'jwt-decode'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 class Letter extends Component {
+
+  state ={
+    campaigns:[]
+  }
+
+  componentWillMount() {
+    let userId = jwtDecode(localStorage.getItem('token')).userId
+    console.log(jwtDecode(localStorage.getItem('token')).userId)
+    axios.get(`/api/letter/${userId}`)
+    .then(results =>{
+      console.log (results)
+      this.setState({
+        campaigns: results.data
+      })
+    })
+  }
+
   render() {
     return(
       <div>
         <div>
-        (enter date)
+        {this.state.campaigns.map(function(campaign){
+          return <div key={campaign.id} className='result'>
+            <div>
+            <p>Date: {campaign.timestamp}</p>
+            <p>Name: {campaign.name}</p>
+            </div>
+            <div>
+              <p> To: (congressperson) </p>
+            </div>
+            <div className="dropdown">
+              <select>
+                <option value="dear">Dear</option>
+                <option value="hello">Hello</option>
+              </select>
+            </div>
+            <div>
+              <p> (congressperson) </p>
+              <p> The purpose of this letter is to inform you of my support for (campaign). As an elected official, please consider my vote and support for (campaign). </p>
+            </div>
+            <div className="dropdown">
+              <select>
+                <option value="sincerely">Sincerely</option>
+                <option value="respects">Respects</option>
+                <option value="regards">Regards</option>
+              </select>
+            </div>
+            <div>
+              <p>{campaign.userId}</p>
+            </div>
+          </div>
+
+        })}
         </div>
-        <div>
-        <p>From: (texter)</p>
-        </div>
-        <div>
-        <p> To: (congressperson) </p>
-        </div>
-        <div class="dropdown">
-          <select>
-            <option value="dear">Dear</option>
-            <option value="hello">Hello</option>
-          </select>
-        </div>
-        <div>
-          <p> (congressperson) </p>
-          <p> The purpose of this letter is to inform you of my support for (campaign). As an elected official, please consider my vote and support for (campaign). </p>
-        </div>
-        <div class="dropdown">
-          <select>
-            <option value="sincerely">Sincerely</option>
-            <option value="respects">Respects</option>
-            <option value="regards">Regards</option>
-          </select>
-        </div>
-        <div>
-          <p>(texter)</p>
-        </div>
-        <button type="submit">Send</button>
+        <button><Link to="/dashboard">Send</Link></button>
       </div>
     )
   }
