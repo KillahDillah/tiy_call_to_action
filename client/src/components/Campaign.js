750b3e27e5a8986d8714fcd3528bd1527cf962c3
Jase Cutler <jasecutler@gmail.com>
2017-10-05 12:18:27 -0700

    formatting

--- client/src/components/Campaign.js
+++ client/src/components/Campaign.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Switch} from 'react-router-dom'
-import {AuthRoute as Route, Authorize} from '../lib/auth'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { AuthRoute as Route, Authorize } from '../lib/auth'
 import CampaignLayout from './CampaignLayout'
 
 import All from './All'
@@ -15,22 +15,22 @@ import Letter from './Letter'
 class Campaign extends Component {
     render() {
         return (
-                <Router>
-                    <CampaignLayout>
-                        <Switch>
+            <Router>
+                <CampaignLayout>
+                    <Switch>
                         <Route exact path="/campaign/:id_campaign" component={All} />
                         <Route exact path="/campaign/:id_campaign/all" component={All} />
                         <Route exact path="/campaign/:id_campaign/updatetexters" component={Update} />
                         <Route exact path="/campaign/:id_campaign/national" component={National} />
                         <Route exact path="/campaign/:id_campaign/representative" component={BySenate} />
-                        <Route exact path="/campaign/:id_campaign/day" component={ByDay}/>
-                        <Route exact path="/campaign/:id_campaign/state" component={ByState}/>
-                        <Route exact path="/campaign/:id_campaign/sendletter" component={Letter}/>
-                        <Route exact path="/campaign/:id_campaign/letter" component={Letter}/>
-                        </Switch>
-                    </CampaignLayout>
-                </Router>	
-                )
+                        <Route exact path="/campaign/:id_campaign/day" component={ByDay} />
+                        <Route exact path="/campaign/:id_campaign/state" component={ByState} />
+                        <Route exact path="/campaign/:id_campaign/sendletter" component={Letter} />
+                        <Route exact path="/campaign/:id_campaign/letter" component={Letter} />
+                    </Switch>
+                </CampaignLayout>
+            </Router>
+        )
     }
 }
 
